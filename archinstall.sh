b5829ba7bcfa5a8db28b24a426c77b46e082e235
Sinval Silva Junior <54493316+kjunda01@users.noreply.github.com>
2025-02-27 23:16:07 -0300

    Atualiza o archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
-set -e
+# Script para instalação e configuração do Arch Linux baseado nos JSONs fornecidos
+# Execute este script como root em um ambiente live do Arch Linux
 
-# Definições gerais
+# Verifica se está sendo executado como root
+if [ "$EUID" -ne 0 ]; then
+    echo "Este script deve ser executado como root!"
+    exit 1
+fi
+
+# Define variáveis de configuração
 HOSTNAME="archlinux"
 TIMEZONE="America/Sao_Paulo"
-LOCALE="en_US.UTF-8"
-KEYMAP="us"
+KEYBOARD="us"
+LANGUAGE="en_US.UTF-8"
 DISK="/dev/nvme0n1"
-INTERFACE="enp6s0"
-ROOT_PASSWORD="112148"
-USERNAME="kjunda01"
-USER_PASSWORD="112148"
+BOOT_PART="${DISK}p1"
+BTRFS_PART="${DISK}p2"
+USER="kjunda01"
+USER_PASS="112148"
+ROOT_PASS="112148"
 
-# Particionamento manual
-wipefs -af $DISK
+# Atualiza o relógio do sistema
+timedatectl set-ntp true
+
+# Particionamento do disco
+echo "Particionando o disco $DISK..."
 parted -s $DISK mklabel gpt
-parted -s $DISK mkpart ESP fat32 1MiB 1GiB
-parted -s $DISK set 1 boot on
+parted -s $DISK mkpart primary fat32 1MiB 1GiB
 parted -s $DISK set 1 esp on
+parted -s $DISK set 1 boot on
 parted -s $DISK mkpart primary btrfs 1GiB 100%
 
-# Formatação
-echo "Formatando partições..."
-mkfs.fat -F32 ${DISK}p1
-mkfs.btrfs -f ${DISK}p2
+# Formatação das partições
+mkfs.fat -F32 $BOOT_PART
+mkfs.btrfs -f $BTRFS_PART
 
-# Criando subvolumes Btrfs
-mount ${DISK}p2 /mnt
+# Configuração do Btrfs com subvolumes
+mount $BTRFS_PART /mnt
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@log
@@ -35,62 +45,90 @@ btrfs subvolume create /mnt/@pkg
 btrfs subvolume create /mnt/@.snapshots
 umount /mnt
 
-# Montagem
-mount -o compress=zstd,subvol=@ ${DISK}p2 /mnt
+# Montagem das partições
+mount -o compress=zstd,subvol=@ $BTRFS_PART /mnt
 mkdir -p /mnt/{boot,home,var/log,var/cache/pacman/pkg,.snapshots}
-mount -o compress=zstd,subvol=@home ${DISK}p2 /mnt/home
-mount -o compress=zstd,subvol=@log ${DISK}p2 /mnt/var/log
-mount -o compress=zstd,subvol=@pkg ${DISK}p2 /mnt/var/cache/pacman/pkg
-mount -o compress=zstd,subvol=@.snapshots ${DISK}p2 /mnt/.snapshots
-mount ${DISK}p1 /mnt/boot
+mount -o compress=zstd,subvol=@home $BTRFS_PART /mnt/home
+mount -o compress=zstd,subvol=@log $BTRFS_PART /mnt/var/log
+mount -o compress=zstd,subvol=@pkg $BTRFS_PART /mnt/var/cache/pacman/pkg
+mount -o compress=zstd,subvol=@.snapshots $BTRFS_PART /mnt/.snapshots
+mount $BOOT_PART /mnt/boot
 
-# Instalação base
-echo "Instalando sistema base..."
-pacstrap /mnt base linux linux-firmware nano vim openssh samba wget curl efibootmgr
-
-# Gerando fstab
-genfstab -U /mnt >> /mnt/etc/fstab
-
-# Configuração do sistema
-echo "Configurando sistema..."
-arch-chroot /mnt ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
-arch-chroot /mnt hwclock --systohc
-echo "$LOCALE UTF-8" > /mnt/etc/locale.gen
-arch-chroot /mnt locale-gen
-echo "LANG=$LOCALE" > /mnt/etc/locale.conf
-echo "KEYMAP=$KEYMAP" > /mnt/etc/vconsole.conf
-echo "$HOSTNAME" > /mnt/etc/hostname
-echo "127.0.0.1   localhost" >> /mnt/etc/hosts
-echo "::1         localhost" >> /mnt/etc/hosts
-echo "127.0.1.1   $HOSTNAME.localdomain $HOSTNAME" >> /mnt/etc/hosts
-
-# Configuração da rede
-echo "Configurando rede..."
-arch-chroot /mnt systemctl enable systemd-networkd
-arch-chroot /mnt systemctl enable systemd-resolved
-cat <<EOF > /mnt/etc/systemd/network/20-wired.network
-[Match]
-Name=$INTERFACE
-
-[Network]
-DHCP=yes
+# Configuração dos mirrors (usando apenas mirrors do Brasil)
+echo "Configurando mirrors brasileiros..."
+cat << EOF > /etc/pacman.d/mirrorlist
+Server = http://archlinux.c3sl.ufpr.br/\$repo/os/\$arch
+Server = https://archlinux.c3sl.ufpr.br/\$repo/os/\$arch
+Server = http://br.mirrors.cicku.me/archlinux/\$repo/os/\$arch
+Server = https://br.mirrors.cicku.me/archlinux/\$repo/os/\$arch
+Server = http://mirror.ufam.edu.br/archlinux/\$repo/os/\$arch
+Server = http://mirror.ufscar.br/archlinux/\$repo/os/\$arch
+Server = https://mirror.ufscar.br/archlinux/\$repo/os/\$arch
+Server = http://mirrors.ic.unicamp.br/archlinux/\$repo/os/\$arch
+Server = https://mirrors.ic.unicamp.br/archlinux/\$repo/os/\$arch
 EOF
 
-# Configuração do usuário root e novo usuário
-echo "Configurando usuários..."
-echo -e "$ROOT_PASSWORD\n$ROOT_PASSWORD" | arch-chroot /mnt passwd root
-echo "Criando usuário $USERNAME..."
-arch-chroot /mnt useradd -m -G wheel -s /bin/bash $USERNAME
-echo -e "$USER_PASSWORD\n$USER_PASSWORD" | arch-chroot /mnt passwd $USERNAME
-echo "%wheel ALL=(ALL) ALL" >> /mnt/etc/sudoers
+# Instalação do sistema base
+pacstrap /mnt base linux linux-firmware
 
-# Instalando bootloader
-echo "Instalando GRUB..."
-arch-chroot /mnt pacman -Sy --noconfirm grub
-arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+# Geração do fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Configuração do sistema instalado via chroot
+arch-chroot /mnt /bin/bash << EOF
+# Configuração de fuso horário
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+hwclock --systohc
+
+# Configuração de locale
+echo "$LANGUAGE" > /etc/locale.gen
+locale-gen
+echo "LANG=$LANGUAGE" > /etc/locale.conf
+echo "KEYMAP=$KEYBOARD" > /etc/vconsole.conf
+
+# Configuração do hostname
+echo "$HOSTNAME" > /etc/hostname
+cat << HOSTS > /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   $HOSTNAME.localdomain $HOSTNAME
+HOSTS
+
+# Instalação do bootloader (Grub)
+pacman -S --noconfirm grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Configuração de swap (simples arquivo de swap)
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+echo "/swapfile none swap sw 0 0" >> /etc/fstab
+
+# Instalação de pacotes adicionais
+pacman -S --noconfirm nano vim openssh samba wget curl pipewire pipewire-pulse hyprland sddm polkit
+
+# Configuração de áudio (PipeWire)
+systemctl enable pipewire pipewire-pulse
+
+# Configuração de rede (DHCP automático)
+pacman -S --noconfirm networkmanager
+systemctl enable NetworkManager
+
+# Configuração do SDDM (greeter)
+systemctl enable sddm
+
+# Configuração de usuário e root
+echo "root:$ROOT_PASS" | chpasswd
+useradd -m -G wheel -s /bin/bash $USER
+echo "$USER:$USER_PASS" | chpasswd
+echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
+
+# Saída do chroot
+exit
+EOF
 
 # Finalização
-echo "Instalação concluída! Rebootando..."
-umount -R /mnt
-reboot
+echo "Instalação concluída! Desmonte as partições e reinicie."
+# Para desmontar manualmente: umount -R /mnt
+# Para reiniciar: reboot
