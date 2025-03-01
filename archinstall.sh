a131442b7a3cdb57b0e147237d6ab18c9b8c3edc
Sinval Junior <sinvaljuniorlms@gmail.com>
2025-03-01 01:41:48 -0300

    altera script

--- archinstall.sh
+++ archinstall.sh
@@ -42,15 +42,13 @@ KEYBOARD=$(select_option "Escolha o layout do teclado:" "${KEYBOARDS[@]}")
 LANGUAGES=("en_US.UTF-8" "pt_BR.UTF-8" "es_ES.UTF-8" "fr_FR.UTF-8")
 LANGUAGE=$(select_option "Escolha o idioma do sistema:" "${LANGUAGES[@]}")
 
-# Descobrir discos disponíveis
+# Detectar discos disponíveis corretamente
 echo "Detectando discos disponíveis..."
-DISKS=($(lsblk -d -n -o NAME,SIZE | awk '{print "/dev/" $1}' | tr '\n' ' '))
-
+DISKS=($(lsblk -d -n -o NAME | awk '{print "/dev/" $1}'))
 if [ ${#DISKS[@]} -eq 0 ]; then
     echo "Nenhum disco detectado. Abortando."
     exit 1
 fi
-
 DISK=$(select_option "Escolha o disco para instalar o sistema:" "${DISKS[@]}")
 
 # Pergunta sobre o usuário
@@ -58,31 +56,17 @@ read -rp "Digite o nome do usuário [padrão: kjunda01]: " USER
 USER=${USER:-kjunda01}
 
 # Pergunta sobre as senhas
-while true; do
-    read -rsp "Digite a senha do usuário: " USER_PASS
-    echo
-    read -rsp "Confirme a senha do usuário: " USER_PASS_CONFIRM
-    echo
-    [ "$USER_PASS" == "$USER_PASS_CONFIRM" ] && break
-    echo "As senhas não coincidem. Tente novamente."
-done
+read -srp "Digite a senha do usuário: " USER_PASS
+echo
+read -srp "Digite a senha do root: " ROOT_PASS
+echo
 
-while true; do
-    read -rsp "Digite a senha do root: " ROOT_PASS
-    echo
-    read -rsp "Confirme a senha do root: " ROOT_PASS_CONFIRM
-    echo
-    [ "$ROOT_PASS" == "$ROOT_PASS_CONFIRM" ] && break
-    echo "As senhas não coincidem. Tente novamente."
-done
-
-# Partições baseadas no disco escolhido
+# Criar partições corretamente
 BOOT_PART="${DISK}1"
 BTRFS_PART="${DISK}2"
 
 # Resumo das configurações
 echo -e "\nConfiguração escolhida:"
-echo "Hostname: archlinux"
 echo "Fuso Horário: $TIMEZONE"
 echo "Teclado: $KEYBOARD"
 echo "Idioma: $LANGUAGE"
@@ -108,6 +92,7 @@ echo "Particionando o disco $DISK..."
 parted -s $DISK mklabel gpt
 parted -s $DISK mkpart primary fat32 1MiB 1GiB
 parted -s $DISK set 1 esp on
+parted -s $DISK set 1 boot on
 parted -s $DISK mkpart primary btrfs 1GiB 100%
 
 # Formatação das partições
@@ -132,4 +117,38 @@ mount -o compress=zstd,subvol=@pkg $BTRFS_PART /mnt/var/cache/pacman/pkg
 mount -o compress=zstd,subvol=@.snapshots $BTRFS_PART /mnt/.snapshots
 mount $BOOT_PART /mnt/boot
 
-echo "Particionamento e montagem concluídos!"
+# Instalação do sistema base
+pacstrap /mnt base linux linux-firmware
+
+# Geração do fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+# Chroot e configuração pós-instalação
+arch-chroot /mnt bash <<EOF
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+hwclock --systohc
+sed -i 's/#$LANGUAGE/$LANGUAGE/' /etc/locale.gen
+locale-gen
+echo "LANG=$LANGUAGE" > /etc/locale.conf
+echo "KEYMAP=$KEYBOARD" > /etc/vconsole.conf
+echo "archlinux" > /etc/hostname
+cat << HOSTS > /etc/hosts
+127.0.0.1   localhost
+::1         localhost
+127.0.1.1   archlinux.localdomain archlinux
+HOSTS
+
+# Configuração do bootloader
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# Criação de usuário
+echo "root:$ROOT_PASS" | chpasswd
+useradd -m -G wheel -s /bin/bash $USER
+echo "$USER:$USER_PASS" | chpasswd
+echo "$USER ALL=(ALL) ALL" >> /etc/sudoers.d/$USER
+chmod 440 /etc/sudoers.d/$USER
+EOF
+
+# Finalização
+echo "Instalação concluída! Desmonte as partições e reinicie."
